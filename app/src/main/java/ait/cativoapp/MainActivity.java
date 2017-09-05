8e309ba933022eaf91f61003c6160127bedcceaa
CliversonObrzut <cliverson.obrzut@tafensw.net.au>
2017-09-05 20:28:53 +1000

    Created the Cativo logo and generated the Toolbar

--- app/src/main/java/ait/cativoapp/MainActivity.java
+++ app/src/main/java/ait/cativoapp/MainActivity.java
@@ -2,6 +2,9 @@ package ait.cativoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,23 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // TODO: Set Cativo logo after designed
+        getSupportActionBar().setIcon(R.drawable.cativo_logo_24px);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // create menu from xml to java objects, add to menu (parent group)
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
